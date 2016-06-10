8d04bcba7bf4e542b33d157357e898d663588fb1
Guillermo <memo.9321@gmail.com>
2016-06-10 14:03:15 -0500

    changes errors

--- admin/php/connect_bd.php
+++ admin/php/connect_bd.php
@@ -1,9 +1,15 @@
 <?php
 
-	$mysql_host = "mysql.thebeerfans.com";
-	$mysql_user = "tbf1495";
-	$mysql_password = "tbf1495tbf";
-	$mysql_database = "beerfans_social";
+	// $mysql_host = "mysql.thebeerfans.com";
+	// $mysql_user = "tbf1495";
+	// $mysql_password = "tbf1495tbf";
+	// $mysql_database = "beerfans_social";
+	// $link = "";
+
+	$mysql_host = "localhost";
+	$mysql_user = "root";
+	$mysql_password = "";
+	$mysql_database = "beerfans";
 	$link = "";
 
 	function connect_base_de_datos (){
